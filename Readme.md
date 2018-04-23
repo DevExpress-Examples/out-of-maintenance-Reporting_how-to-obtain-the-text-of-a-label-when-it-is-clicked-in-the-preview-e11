c11f7237d15b9e6ab9ff8d9f4c6e1bbbfa64d7ed
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to obtain the text of a label when it is clicked in the preview
+
+
+<p>This example demonstrates how to obtain a label's text when it is clicked in preview.</p>
+
+<br/>
+
+
---
+++ VB/XtraReport1.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UI
+' ...
+
+Namespace PreviewLabelClick
+	Partial Public Class XtraReport1
+		Inherits DevExpress.XtraReports.UI.XtraReport
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub xrLabel1_PreviewClick(ByVal sender As Object, ByVal e As PreviewMouseEventArgs) Handles xrLabel1.PreviewClick
+			MessageBox.Show(e.Brick.Text)
+		End Sub
+
+		Private Sub xrLabel2_PreviewDoubleClick(ByVal sender As Object, ByVal e As PreviewMouseEventArgs) Handles xrLabel2.PreviewDoubleClick
+			MessageBox.Show(e.Brick.Text)
+		End Sub
+
+	End Class
+End Namespace
