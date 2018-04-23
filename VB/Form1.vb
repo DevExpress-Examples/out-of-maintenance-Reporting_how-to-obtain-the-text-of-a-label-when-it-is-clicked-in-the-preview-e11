8dbd8be220155d322d1249114bed36cea62f5f2a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Form1.vb
@@ -0,0 +1,17 @@
+Imports System
+Imports System.Windows.Forms
+' ...
+
+Namespace PreviewLabelClick
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			CType(New XtraReport1(), XtraReport1).ShowPreview()
+		End Sub
+
+	End Class
+End Namespace
